7382e1a69e0dbbde558eadcf0429beef69c211d9
Jim Chen <jimmychimmyy@gmail.com>
2014-11-07 08:35:41 -0800

    KandiTAG preMessaging

---
+++ KandiTAG/ChatTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ChatTableViewCell.h
+//  KandiTAG
+//
+//  Created by Jim Chen on 11/4/14.
+//  Copyright (c) 2014 Jim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatTableViewCell : UITableViewCell
+
+@end
---
+++ KandiTAG/ChatTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatTableViewController.h
+//  KandiTAG
+//
+//  Created by Jim Chen on 10/30/14.
+//  Copyright (c) 2014 Jim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatTableViewController : UITableViewController
+
+@end
