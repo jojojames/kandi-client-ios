26eaad5c75f7d9a8f926770f16e759f9339dca85
Jim Chen <jimmychimmyy@gmail.com>
2014-11-24 12:42:21 -0800

    still need to set up chat table cells

---
+++ KandiTAG/MessageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewCell.h
+//  KandiTAG
+//
+//  Created by Jim Chen on 11/24/14.
+//  Copyright (c) 2014 Jim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageTableViewCell : UITableViewCell
+
+@end
---
+++ KandiTAG/MessageTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MessageTableViewCell.m
+//  KandiTAG
+//
+//  Created by Jim Chen on 11/24/14.
+//  Copyright (c) 2014 Jim. All rights reserved.
+//
+
+#import "MessageTableViewCell.h"
+
+@implementation MessageTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
